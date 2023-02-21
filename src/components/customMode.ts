487108906002a933759fb5ffc813cf61c15bc70f
Tanner <devtanc@gmail.com>
2023-02-20 22:53:39 -0700

    also include weird hyphen character

--- src/components/customMode.ts
+++ src/components/customMode.ts
@@ -14,12 +14,12 @@ export class CustomHighlightRules extends globalThis.ace.acequire(
       start: [
         {
           token: "scripture",
-          regex: `(?<=^|\\s|\\W)([1-4] )?(${books})\\.? [\\d:-]+(, \\d{1,3}(-\\d{1,3})?(?:(?!:)))*(?=$|\\s|\\W)`,
+          regex: `(?<=^|\\s|\\W)([1-4] )?(${books})\\.? [\\d:–-]+(, \\d{1,3}([–-]\\d{1,3})?(?:(?!:)))*(?=$|\\s|\\W)`,
         },
         {
           token: "scripture.nobook",
           regex:
-            /\d{1,3}:\d{1,3}(-\d{1,3}(?:(?!:))|(, \d{1,3}(-\d{1,3})?(?:(?!:))))*(?=$|\s|\W)/,
+            /\d{1,3}:\d{1,3}([–-]\d{1,3}(?:(?!:))|(, \d{1,3}([–-]\d{1,3})?(?:(?!:))))*(?=$|\s|\W)/,
         },
         {
           token: "keyword.namechrist",
